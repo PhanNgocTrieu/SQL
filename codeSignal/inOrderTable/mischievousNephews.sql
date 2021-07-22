e3f6096ac09e11740cb1a6a6f35a2cf145c44389
PhanNgocTrieu <bettertogetheronit@gmail.com>
2021-07-22 22:57:53 +0700

    mischievous Newphews

--- codeSignal/inOrderTable/mischievousNephews.sql
+++ codeSignal/inOrderTable/mischievousNephews.sql
@@ -25,3 +25,10 @@ It is guaranteed that all entries of mischief are unique.
 
 */
 
+CREATE PROCEDURE mischievousNephews()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+    SELECT weekday(mischief_date) AS weekday, mischief_date, author,title
+    FROM mischief
+    ORDER BY weekday,field(author,"Huey","Dewey","Louie"),mischief_date,title;
+END
